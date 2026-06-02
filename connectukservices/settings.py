bae14963fcd219e563bf7a28b31ceb82d396fa3d
ahmadnawaz <ahn63400@gmail.com>
2026-06-02 21:28:46 +0500

    Final

--- connectukservices/settings.py
+++ connectukservices/settings.py
@@ -150,14 +150,14 @@ LOGOUT_REDIRECT_URL = 'home'
 # 7. EMAIL CONFIGURATION (SMTP - Brevo Provider)
 # ==============================================================================
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Environment variables se value utha rahe hain, agar na mile toh default values use hongi
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-relay.brevo.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'ad4dca001@smtp-brevo.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-relay.brevo.com')
+# EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'ad4dca001@smtp-brevo.com')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 30  # Connection timeout 30s
+# EMAIL_USE_TLS = True
+# EMAIL_TIMEOUT = 30  # Connection timeout 30s
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'ConnectUK Services <ahn63400@gmail.com>')
